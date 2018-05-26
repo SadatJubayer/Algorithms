1ba40a42ee5fc8c8bc134e01381a13a350ef67d2
S. M. Jubayer <sadatjubayer@gmail.com>
2018-05-25 19:19:04 -0700

    Update 01 decimalToBinary.cpp

--- 00 Basic/01 decimalToBinary.cpp
+++ 00 Basic/01 decimalToBinary.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-using namespace std;
-
-int main()
+#include<iostream.h>
+#include<conio.h>
+void main()
 {
-    cout << "Hello world";
+	clrscr();
+	long int binnum, decnum=0, i=1, rem;
+	cout<<"Enter any binary number : ";
+	cin>>binnum;
+	while(binnum!=0)
+	{
+		rem=binnum%10;
+		decnum=decnum+rem*i;
+		i=i*2;
+		binnum=binnum/10;
+	}
+	cout<<"Equivalent decimal value = "<<decnum;
+	getch();
 }
