d5ac7ef5818365e559720fc877a24b466b2a3a6d
Sadat Jubayer <sadatjubayer@gmail.com>
2018-06-25 07:58:39 -0700

    00Basic completed

--- 00 Basic/09. insertionSort.cpp
+++ 00 Basic/09. insertionSort.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void selection(int arr[], int n)
+void insertion(int arr[], int n)
 {
-    for (int i=0; i<10; i++)
+    for (int i=0; i<n; i++)
     {
-        for (int j=i+1; j<10; j++)
-            if (arr[i] > arr[j])
-                swap(arr[i],arr[j]);
+        int x = arr[i];
+        int j = i-1;
+        while (j >= 0 && arr[j]>x)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr [j + 1] = x;
     }
 }
 
@@ -16,12 +21,13 @@ void showArray (int arr[], int n)
     for (int i=0; i<n; i++)
         cout << arr [i] << " ";
 }
+
 int main()
 {
     int arr [] = {15,25,13,6,5,7,98,8,2,55};
     int len = sizeof(arr)/sizeof(arr[0]);
 
-    selection(arr,len);
+    insertion(arr,len);
     cout << "Sorted array: ";
     showArray(arr, len);
 
--- 00 Basic/10. bubbleSort.cpp
+++ 00 Basic/10. bubbleSort.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void selection(int arr[], int n)
+void bubble(int arr[], int n)
 {
-    for (int i=0; i<10; i++)
+    int temp;
+    for (int i=0; i<n-1; i++)
     {
-        for (int j=i+1; j<10; j++)
-            if (arr[i] > arr[j])
-                swap(arr[i],arr[j]);
+        for (int j=0; j<n-i-1; j++)
+            if (arr[j+1] < arr[j])
+                swap(arr[j+1],arr[j]);
     }
 }
+
 
 void showArray (int arr[], int n)
 {
     for (int i=0; i<n; i++)
         cout << arr [i] << " ";
 }
+
 int main()
 {
     int arr [] = {15,25,13,6,5,7,98,8,2,55};
     int len = sizeof(arr)/sizeof(arr[0]);
 
-    selection(arr,len);
+    bubble(arr,len);
     cout << "Sorted array: ";
     showArray(arr, len);
 
 }
+
